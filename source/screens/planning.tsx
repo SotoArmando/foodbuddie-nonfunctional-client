27019db0b4edebbf04fe52c5601a8a57c2790360
Armando Soto <armando29@live.com>
2025-08-22 23:28:03 +0000

    Undo

--- source/screens/planning.tsx
+++ source/screens/planning.tsx
@@ -56,7 +56,7 @@ const useDayListContainer = (props) => {
         handleClick={updateDay2}
         progress={progress}
         index={id}
-    />, []);
+    />, [day]);
 
     const newLocal_1 = useCallback((event) => {
         const newLocal = Math.round(event.nativeEvent.contentOffset.x / scale(((24 + 31) * 7)));
@@ -463,7 +463,7 @@ function PlannerDaysRowElementW({
         PlannerDaysRowElementDayActive,
         PlannerDaysRowElementDot,
     } = useComponentStyles('Planner');
-    // console.log('am rerendering', dayLabel, dayNumber)
+    console.log('am rerendering', dayLabel, dayNumber)
 
     const newLocal = useCallback(async () => handleClick(index), [current]);
     const focus = useMemo(() => current && <Animated.View style={[{ position: 'absolute', left: scale(-2), right: scale(-1), top: scale(4), bottom: scale(-8), borderRadius: scale(16) }, animatedStyle, { display: current ? 'flex' : 'none' }, {
