1622108e9719a98a0ad08dc8faf2ea6441b7b7f6
Armando Soto <armando29@live.com>
2025-08-23 01:33:37 +0000

    Default ripple color update

--- source/components/CommonRectButton.tsx
+++ source/components/CommonRectButton.tsx
@@ -1,7 +1,7 @@
 import { TouchableNativeFeedback, TouchableNativeFeedbackProps } from "react-native";
 import { BaseButton, BaseButtonProps, RectButton, RectButtonProps } from "react-native-gesture-handler";
 
-export const CommonRectButton = ({ rippleColor = 'rgba(216,38,106,0.4)',...props }: BaseButtonProps) => {
+export const CommonRectButton = ({ rippleColor = '#E0DEDA',...props }: BaseButtonProps) => {
     return <BaseButton rippleColor={rippleColor} {...props} >
         <>
          {props.children}
